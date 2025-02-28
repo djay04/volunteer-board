f0284e88e887a3d6b3a18ef23ccdd5ad8502af46
Khaled <117534908+djay04@users.noreply.github.com>
2025-02-28 15:42:33 -0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import OpportunitiesList from "./OpportunitiesList";
+import "./styles.css";
+
+function App() {
+  return (
+    <div className="app-container">
+      <h1>Volunteer Opportunities 🌍</h1>
+      <OpportunitiesList />
+    </div>
+  );
+}
+export default App;
